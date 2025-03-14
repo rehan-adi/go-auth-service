d5d852117c9d1b467612ef8b3c8b29eb4fe1c468
Rehan <rehanalire52@gmail.com>
2025-03-14 23:53:29 +0530

    feat: Add signup data validation with custom error handling

---
+++ internal/validators/auth.go
@@ -0,0 +1,21 @@
+package validators
+
+import "github.com/go-playground/validator/v10"
+
+type SignupValidator struct {
+	Username string `json:"username" validate:"required,min=2,max=15"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+var validate = validator.New()
+
+func ValidateSignupData(input SignupValidator) map[string]string {
+	errs := make(map[string]string)
+	if err := validate.Struct(input); err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			errs[e.Field()] = "Invalid " + e.Field()
+		}
+	}
+	return errs
+}
