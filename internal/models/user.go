df5e27d795ff50050686f506a5e880856fccb03d
Rehan <rehanalire52@gmail.com>
2025-04-18 17:53:06 +0530

    refactor(models): remove redundant ID field from User struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,14 +1,11 @@
 package models
 
 import (
-	//	"time"
-
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	ID       int    `gorm:"primaryKey"`
 	Email    string `gorm:"uniqueIndex;not null"`
 	Username string `gorm:"not null"`
 	Password string `gorm:"not null"`
